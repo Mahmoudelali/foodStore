2ce6fa8dc3bca3349435d22e5a8007a537302fd6
Mahmoud_elali <mahmud.elali98@gmail.com>
2023-08-06 02:07:28 +0300

    edited searchbar styles

--- app/components/searchInput.js
+++ app/components/searchInput.js
@@ -1,18 +1,46 @@
-import { TextInput, View } from "react-native"
+import { TextInput, View, StyleSheet } from 'react-native';
 import { EvilIcons } from '@expo/vector-icons';
-import {
-
-    Feather,
-  } from "@expo/vector-icons";
+import { Feather } from '@expo/vector-icons';
 const SearchInput = () => {
-return(
-    <View className="flex-row bg-white p-[5%] mb-[2%] inset-shadow(2px 4px 8px 2px black)">
-        <Feather name="search" size={20} color={"#13d0ca"} />
-					<TextInput placeholder="Search Coupway.." className="color-gray w-[50%]" />
-					<View className= "h-[100%] w-[1] bg-gray-300 ml-5"  />
-					<EvilIcons name="location" size={24} color="#13d0ca" />
-					<TextInput placeholder="Location" className="color-gray w-[23%]"  />
-				  </View>
-)
-}
-export default SearchInput
+	return (
+		<View>
+			<View className="flex-row bg-white mb-2  mx-auto">
+				<View className="basis-3/5">
+					<Feather
+						name="search"
+						size={20}
+						color={'#13d0ca'}
+						style={style.searchIcons}
+					/>
+					<TextInput
+						placeholder="Search Coupway.."
+						className="color-gray p-4 relative pl-7"
+					/>
+				</View>
+
+				<View className="h-[100%] w-[1] bg-gray-300" />
+				<View className="basis-2/5">
+					<EvilIcons
+						name="location"
+						size={24}
+						color="#13d0ca"
+						style={style.searchIcons}
+					/>
+					<TextInput
+						style={style.searchIcons}
+						placeholder="Location"
+						className="color-gray relative pl-7"
+					/>
+				</View>
+			</View>
+		</View>
+	);
+};
+const style = StyleSheet.create({
+	searchIcons: {
+		position: 'absolute',
+		top: 7,
+		left: 5,
+	},
+});
+export default SearchInput;
