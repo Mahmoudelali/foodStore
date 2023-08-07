2ee76c1b75ad0a72991cafb8585d1b38dbdc7e29
Mahmoud_elali <mahmud.elali98@gmail.com>
2023-08-07 21:15:41 +0300

    t123

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-import { View, TextInput, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Home from './screens/home';
 import Search from './screens/search.js';
 import Login from './screens/Login';
@@ -11,25 +11,9 @@ import Notifications from './screens/WishList';
 import AddressBookCard from './components/AddressBookCard';
 import AddressBookEdit from './components/AddressBook_edit';
 import ChangePassword from './screens/ChangePassword';
+// import Welcome from './screens/Welcome';
 
 const Tab = createBottomTabNavigator();
-const screenOptions = {
-	tabBarShowLabel: false,
-	headerShown: true,
-	headerStyle: {
-		backgroundColor: '#13d0ca',
-	},
-	tabBarStyle: {
-		position: 'absolute',
-		bottom: 0,
-		right: 0,
-		left: 0,
-		elevation: 0,
-		height: 40,
-		backgroundColor: '#13d0ca',
-	},
-};
-
 const Stack = createNativeStackNavigator();
 
 export default function Page() {
@@ -38,24 +22,20 @@ export default function Page() {
 	return (
 		<>
 			<Stack.Navigator initialRouteName={'ChangePassword'}>
+				{/* <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} /> */}
 				<Stack.Screen name="Login" component={Login} />
-				<Stack.Screen name="AddressBook" component={AddressBookCard} />
 				<Stack.Screen name="Register" component={Register} />
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Search" component={Search} />
+				<Stack.Screen name="AddressBook" component={AddressBookCard} />
 				<Stack.Screen name="ProductScreen" component={ProductScreen} />
 				<Stack.Screen name="WishList" component={Notifications} />
-				<Stack.Screen
-					name="ChangePassword"
-					component={ChangePassword}
-				/>
+				<Stack.Screen name="ChangePassword" component={ChangePassword} />
+				<Stack.Screen name="Nav" component={Nav} options={{ headerShown: false }} />
 				<Stack.Screen
 					options={{ headerShown: true, title: 'edit address' }}
 					name="AddressBookEdit"
 					component={AddressBookEdit}
-				/>
-				<Stack.Screen
-					name="Nav"
-					component={Nav}
-					options={{ headerShown: false }}
 				/>
 			</Stack.Navigator>
 		</>
