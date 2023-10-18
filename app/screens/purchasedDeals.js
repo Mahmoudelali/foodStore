2ef2329d4e9955423de96dfda2eb86c56ae6fd83
Mahmoud_elali <mahmud.elali98@gmail.com>
2023-10-18 22:02:16 +0300

    qr modification

--- app/screens/purchasedDeals.js
+++ app/screens/purchasedDeals.js
@@ -1,17 +1,23 @@
-import React from "react";
-import { ScrollView } from "react-native-gesture-handler";
-import OrderBox from "../components/UsedBox";
-import NotFound from "../components/NotFound";
+import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import OrderBox from '../components/UsedBox';
+import NotFound from '../components/NotFound';
 
 export default function PurchasedDeals({ route }) {
-  const { data } = route.params;
-  return (
-    <ScrollView className="pt-3">
-      {data.length === 0 ? (
-        <NotFound label="No purchased deals found" />
-      ) : (
-        data.map((off) => <OrderBox key={off.id} {...off} status="active" />)
-      )}
-    </ScrollView>
-  );
+	const { data } = route.params;
+	console.log('data', JSON.stringify(data, null, 2));
+	return (
+		<ScrollView className="pt-3">
+			{data.length === 0 ? (
+				<NotFound label="No purchased deals found" />
+			) : (
+				data.map((offer) => {
+					console.log('offer', JSON.stringify(offer, null, 2));
+					return (
+						<OrderBox key={offer.id} {...offer} status="active" />
+					);
+				})
+			)}
+		</ScrollView>
+	);
 }
